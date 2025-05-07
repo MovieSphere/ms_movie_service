0c10a0ceea8346ea2e71835a8b14923e0f2e5fea
Kathia Medina Rodriguez <kmedinar1@upao.edu.pe>
2025-05-07 17:30:50 -0500

    fitst commit

---
+++ src/test/java/com/upao/infraestructura/ms_movie_service/MsMovieServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upao.infraestructura.ms_movie_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMovieServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
